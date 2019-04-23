c530ab3dbe93996fa4b447aa3bf188397a8398f7
Riedler <david.riedler@gelse.net>
2019-04-23 10:38:41 +0200

    fused YTD with Converter. added default settings file

--- YT downloader.py
+++ YT downloader.py
@@ -1,4 +1,15 @@
 import pytube, os, sys, io, mutagen, json
+#
+#POSSIBLE ARGS:
+#	--conf
+#		triggers configuration and then quits
+#	--help
+#		shows help
+#	--overwrite
+#		download and convert videos even if existent.
+#	<link>
+#		a link. (youtube only) 
+#
 class Vid(pytube.YouTube):
 	def ineed(self):
 		self.name=self.title
@@ -13,23 +24,39 @@ def singvid(link):
 	print("All streams:\n",streams.all())
 	stream=streams.first()
 	print("Selected Stream:\n",stream)
-	stream.download(output_path=conf.curdir,filename=strip(yt.title,yt.i))
+	filename=strip(yt.title,yt.i)
+	stream.download(output_path=conf.curdir,filename=filename)
+	print("Converting...")
+	convert(conf.curdir+"/"+filename)
 	print("Finished")
 def stuff(vid):
 	vid.ineed()
 	sys.stdout.write("\033[s"+strip(vid.title)[0])
 	sys.stdout.flush()
+	filename=strip(vid.title,vid.i)
+	print("\033[7m[Downloading]\033[0m"," "*10,end="\033[24D")
+	sys.stdout.flush()
 	try:	
-		vid.streams.first().download(output_path=conf.curdir,filename=strip(vid.title,vid.i))
+		vid.streams.first().download(output_path=conf.curdir,filename=filename)
 	except Exception as e:
 		if e is KeyboardInterrupt:
 			quit()
 		else:
-			print(" \033[7m\033[31m[Failed]\033[0m",e,end=" "*10+"\n\r")
+			print(" \033[7m\033[31m[Failed1]\033[0m",e,end=" "*10+"\n\r")
 			conf.stats[False]+=1
 	else:
-		sys.stdout.write(" \033[7m\033[32m[Finished]\033[0m\n\r")
-		conf.stats[True]+=1
+		print("\033[7m[Converting]\033[0m"," "*10,end="\033[23D")
+		sys.stdout.flush()
+		try:
+			convert(conf.curdir+"/"+filename,ext=None)
+		except KeyboardInterrupt:
+			quit()
+		#except Exception as e:
+		#	print(" \033[7m\033[31m[Failed2]\033[0m",e,end=" "*10+"\n\r")
+		#	conf.stats[False]+=1
+		else:
+			sys.stdout.write(" \033[7m\033[32m[Finished]\033[0m          \n\r")
+			conf.stats[True]+=1
 	sys.stdout.flush()
 def playlist(link,files):
 	#https://www.youtube.com/playlist?list=PLk5KkYG8je9DsAgKQcRrT9wOlTHiJmORl
@@ -52,7 +79,7 @@ def playlist(link,files):
 				except KeyboardInterrupt:
 					raise KeyboardInterrupt()
 				except Exception as e:
-					sys.stdout.write("\033[7m\033[31m[Failed]\033[0m"+" "*10+"\n\r")
+					sys.stdout.write("\033[7m\033[31m[Failed3]\033[0m"+" "*16+"\n\r")
 				else:
 					stuff(vid)
 			else:
@@ -117,7 +144,7 @@ class Config():
 		found=False
 		for dirpath, dirnames, filenames in os.walk(path):
 			for f in filenames:
-				if f.startswith(".rytdconf"):		#No guarantee for anything, but because of how i do this, you can specify multiple config files in multiple ways.
+				if f.startswith(".rytdconf"):
 					found=True
 					conffile=open(dirpath+"/"+f,"r")
 					try:
@@ -150,6 +177,49 @@ class Config():
 			json.dump(sett,conffile)
 		finally:
 			conffile.close()
+def convert(f,ext):
+	if f.endswith(".mp4") or f.endswith(".webm") or f.endswith(".mkv"):
+		fn,ext=os.path.splitext(f)
+	elif ext==None:
+		for ext in (".mp4",".mkv",".webm"):
+			tryagain=False
+			success=False
+			try:
+				convert(f,ext)
+			except OSError:
+				tryagain=True
+			else:
+				success=True
+			if success or not tryagain:
+				break
+		if not success:
+			raise OSError("File doesn't exist. "+f)
+		else:
+			return
+	else:
+		f+=ext
+	if not os.path.exists(f):
+		raise OSError("File doesn't exist. "+f)
+	elif os.path.isfile(f):
+		dirpath,f=os.path.split(f)
+	else:
+		if os.path.isdir(f):
+			raise TypeError("Expected a file, got a directory.")
+		elif os.path.islink(f):
+			raise TypeError("Expected a file, got a Link.")
+		elif os.path.ismount(f):
+			raise TypeError("Expected a file, got a mount point.")
+	if "\u2020" in f:
+		i,f=os.path.splitext(f)[0].split("\u2020")
+		command="ffmpeg -v 0 -i \""+dirpath+"/"+i+"\u2020"+f+ext+"\" -vn -y -metadata comment=\""+i+"\" \""+dirpath+"/"+f+".ogg\""
+		excode=os.system(command)
+		os.remove(dirpath+"/"+i+"\u2020"+f+ext)
+		if excode==0:
+			pass
+		elif excode==2:
+			raise KeyboardInterrupt()
+		else:
+			raise Exception("Exited with errcode \033[1m"+str(excode)+"\033[0m\n"+str(command))
 def main():
 	global conf
 	conf=Config()
