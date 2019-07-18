cdfd3e9391e1d3f2064d057683f3f4f74274ac11
rasphi <pdamianik@student.tgm.ac.at>
2019-07-18 16:16:28 +0200

    Optimized mysql-connector Installation outputs

--- resolve.py
+++ resolve.py
@@ -1,7 +1,11 @@
 import socket, sys, os, sqlite3
 
-if os.system(sys.executable + " -m pip install mysql-connector") != 0:
+print("Trying to install mysql-connector")
+
+if os.system(sys.executable + " -m pip -q install --upgrade mysql-connector") != 0:
     print("Failed to install required package \"mysql-connector\" via pip.")
     sys.exit()
+
+print("mysql-connector ist ready")
 
 import mysql
