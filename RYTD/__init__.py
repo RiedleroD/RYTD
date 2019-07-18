a92e558202921ac9dfe0ce7bb51988d6ebed5956
rasphi <pdamianik@student.tgm.ac.at>
2019-07-18 17:45:02 +0200

    Added pytube, mutagen and youtube_dl to __init__

--- RYTD/__init__.py
+++ RYTD/__init__.py
@@ -2,11 +2,11 @@ import sys, os
 
 print("Trying to install requirements")
 
-requirements = ["mysql-connector"]
+required_packages = ["pytube", "mutagen", "youtube_dl"]
 
-for requirement in requirements:
-    print("Trying to install \"" + requirement + "\")
+for requirement in required_packages:
+    print("Trying to install \"" + requirement + "\"")
     if os.system(sys.executable + " -m pip -q install --upgrade " + requirement) != 0:
         print("Failed to install required package \"" + requirement + "\" via pip.")
-        sys.exit()
+        sys.exit(1)
     print("\"" + requirement + "\" ist ready")
--- RYTD/resolve.py
+++ RYTD/resolve.py
@@ -1,12 +1,5 @@
 import socket, sys, os, sqlite3
 
-print("Trying to install mysql-connector")
-
-if os.system(sys.executable + " -m pip -q install --upgrade mysql-connector") != 0:
-    print("Failed to install required package \"mysql-connector\" via pip.")
-    sys.exit()
-
-print("mysql-connector ist ready")
-
-import mysql
+if __name__ == '__main__':
+    pass
 
